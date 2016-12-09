49b35be08f356435291c8dd450a3a04e05177290
codeyu <codeyu@gmail.com>
2016-12-09 11:08:21 +0800

    Refactoring RedisStorageExtensions

--- Hangfire.Redis.StackExchange/RedisStorageExtensions.cs
+++ Hangfire.Redis.StackExchange/RedisStorageExtensions.cs
@@ -17,31 +17,38 @@
 using System;
 using Hangfire.Annotations;
 using Hangfire.Redis;
-
+using StackExchange.Redis;
 namespace Hangfire
 {
     public static class RedisStorageExtensions
     {
         public static IGlobalConfiguration<RedisStorage> UseRedisStorage(
-            [NotNull] this IGlobalConfiguration configuration,
-            [NotNull] string nameOrConnectionString)
+            [NotNull] this IGlobalConfiguration configuration)
         {
             if (configuration == null) throw new ArgumentNullException(nameof(configuration));
-            if (nameOrConnectionString == null) throw new ArgumentNullException(nameof(nameOrConnectionString));
-
-            var storage = new RedisStorage(nameOrConnectionString);
+            var storage = new RedisStorage();
             return configuration.UseStorage(storage);
         }
 
         public static IGlobalConfiguration<RedisStorage> UseRedisStorage(
             [NotNull] this IGlobalConfiguration configuration,
+            [NotNull] ConnectionMultiplexer connectionMultiplexer,
+            RedisStorageOptions options = null)
+        {
+            if (configuration == null) throw new ArgumentNullException(nameof(configuration));
+            if (connectionMultiplexer == null) throw new ArgumentNullException("connectionMultiplexer");
+            var storage = new RedisStorage(connectionMultiplexer, options);
+            return configuration.UseStorage(storage);
+        }
+
+
+        public static IGlobalConfiguration<RedisStorage> UseRedisStorage(
+            [NotNull] this IGlobalConfiguration configuration,
             [NotNull] string nameOrConnectionString, 
-            [NotNull] RedisStorageOptions options)
+            RedisStorageOptions options = null)
         {
             if (configuration == null) throw new ArgumentNullException(nameof(configuration));
             if (nameOrConnectionString == null) throw new ArgumentNullException(nameof(nameOrConnectionString));
-            if (options == null) throw new ArgumentNullException(nameof(options));
-
             var storage = new RedisStorage(nameOrConnectionString, options);
             return configuration.UseStorage(storage);
         }
