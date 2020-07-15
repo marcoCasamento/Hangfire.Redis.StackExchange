ad98ce34676118241495950e1c6af880b1d32b12
Никита Тимофеев <nikita.t@ati.su>
2020-07-15 19:09:56 +0300

    Fix lost subscription when BackgroundJobServer being stopped and then recreated

--- Hangfire.Redis.StackExchange/RedisSubscription.cs
+++ Hangfire.Redis.StackExchange/RedisSubscription.cs
@@ -10,36 +10,28 @@ namespace Hangfire.Redis
     internal class RedisSubscription : IServerComponent
 #pragma warning restore 618
     {
-        private readonly ManualResetEvent _mre = new ManualResetEvent(false);
-        private readonly RedisStorage _storage;
-        private readonly ISubscriber _subscriber;
+        private readonly AutoResetEvent _resetEvent = new AutoResetEvent(false);
 
         public RedisSubscription([NotNull] RedisStorage storage, [NotNull] ISubscriber subscriber)
         {
-            _storage = storage ?? throw new ArgumentNullException(nameof(storage));
-            Channel = _storage.GetRedisKey("JobFetchChannel");
+            if (storage == null) throw new ArgumentNullException(nameof(storage));
+            if (subscriber == null) throw new ArgumentNullException(nameof(subscriber));
 
-            _subscriber = subscriber ?? throw new ArgumentNullException(nameof(subscriber));
-            _subscriber.Subscribe(Channel, (channel, value) => _mre.Set());
+
+            Channel = storage.GetRedisKey("JobFetchChannel");
+
+            subscriber.Subscribe(Channel, (channel, value) => _resetEvent.Set());
         }
 
         public string Channel { get; }
 
         public void WaitForJob(TimeSpan timeout, CancellationToken cancellationToken)
         {
-            _mre.Reset();
-            WaitHandle.WaitAny(new[] { _mre, cancellationToken.WaitHandle }, timeout);
+            WaitHandle.WaitAny(new[] {_resetEvent, cancellationToken.WaitHandle}, timeout);
         }
 
         void IServerComponent.Execute(CancellationToken cancellationToken)
         {
-            cancellationToken.WaitHandle.WaitOne();
-
-            if (cancellationToken.IsCancellationRequested)
-            {
-                _subscriber.Unsubscribe(Channel);
-                _mre.Dispose();
-            }
         }
     }
 }
